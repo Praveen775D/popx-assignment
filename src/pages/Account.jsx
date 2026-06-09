542bd37cdc6557cc1e69f1f369b2f7bb709a3ba1
Praveen775D <upraveen776@gmail.com>
2026-06-09 20:25:28 +0530

    Implement account settings screen

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,39 @@
 import MobileContainer from "../components/MobileContainer";
+import "../styles/account.css";
 
 export default function Account() {
   return (
     <MobileContainer>
-      <h1>Account Page</h1>
+      <div className="account-header">
+        Account Settings
+      </div>
+
+      <div className="account-body">
+        <div className="profile-section">
+          <img
+            src="https://i.pravatar.cc/100"
+            alt="profile"
+          />
+
+          <div>
+            <div className="profile-name">
+              Marry Doe
+            </div>
+
+            <div className="profile-email">
+              Marry@example.com
+            </div>
+          </div>
+        </div>
+
+        <p className="profile-description">
+          Lorem Ipsum Dolor Sit Amet,
+          Consetetur Sadipscing Elitr,
+          Sed Diam Nonumy Eirmod Tempor
+          Invidunt Ut Labore Et Dolore
+          Magna Aliquyam Erat, Sed Diam.
+        </p>
+      </div>
     </MobileContainer>
   );
 }
